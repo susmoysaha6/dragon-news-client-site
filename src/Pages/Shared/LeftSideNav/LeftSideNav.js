62621e14af1e36673b643bdf2288c136844553e7
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-18 16:38:24 +0600

    All routes setup

---
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h3>Left Side nav</h3>
+        </div>
+    );
+};
+
+export default LeftSideNav;
---
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h3>Right Side</h3>
+        </div>
+    );
+};
+
+export default RightSideNav;
