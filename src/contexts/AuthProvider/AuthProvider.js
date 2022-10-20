bbe199e639247019341a6643071b0d579b1dd7f8
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-20 16:08:13 +0600

    integrate logout

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -10,7 +10,11 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState();
     const providerLogin = (provider) => {
         return signInWithPopup(auth, provider);
-    }
+    };
+
+    const logOut = () => {
+        return signOut(auth);
+    };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -23,7 +27,7 @@ const AuthProvider = ({ children }) => {
 
     }, [])
 
-    const authInfo = { user, providerLogin };
+    const authInfo = { user, providerLogin, logOut };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
