9bb6f8ee76300ad409cc96362e1b3e2b92a2702e
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-21 22:41:00 +0600

    Change API and deployed on firebase

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:5000/news-categories')
+        fetch('https://dragon-news-server-swart.vercel.app/news-categories')
             .then(res => res.json())
             .then(data => setCategories(data))
     }, [])
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -17,17 +17,17 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://dragon-news-server-swart.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({ params }) => fetch(`https://dragon-news-server-swart.vercel.app/${params.id}`)
             },
             {
                 path: '/news/:id',
                 element: <PrivateRoutes><News></News></PrivateRoutes>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({ params }) => fetch(`https://dragon-news-server-swart.vercel.app/${params.id}`)
             },
             {
                 path: '/login',
